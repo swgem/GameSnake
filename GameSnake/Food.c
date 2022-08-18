22012c7bea6c6ab51f955bc817817bb9e0eaccbb
Eric <eric_h_y@hotmail.com>
2022-08-17 22:37:54 -0300

    Change x y food attributes to pos_x pos_y

--- GameSnake/Food.c
+++ GameSnake/Food.c
@@ -13,13 +13,13 @@ void change_food_position(FOOD* food, int x_lim, int y_lim){
 	int r_x = rand();
 	int r_y = rand();
 
-	int x_old = food->x;
-	int y_old = food->y;
+	int x_old = food->pos_x;
+	int y_old = food->pos_y;
 
-	food->x = (r_x % (x_lim + 1));
-	food->y = (r_y % (y_lim + 1));
+	food->pos_x = (r_x % (x_lim + 1));
+	food->pos_y = (r_y % (y_lim + 1));
 
-	if ((food->x == x_old) && (food->y == y_old)) {
+	if ((food->pos_x == x_old) && (food->pos_y == y_old)) {
 		change_food_position(food, x_lim, y_lim);
 	}
 }
@@ -30,7 +30,7 @@ bool check_collision_snake_food(SNAKE* snake, FOOD* food, SNAKE_SEG* ret_collide
 	SNAKE_SEG* iter = &snake->head;
 	while (iter->next_seg != NULL) {
 		iter = iter->next_seg;
-		if ((food->x == iter->pos_x) && (food->y == iter->pos_y)) {
+		if ((food->pos_x == iter->pos_x) && (food->pos_y == iter->pos_y)) {
 			collided = true;
 			ret_collided_seg = iter;
 			break;
@@ -39,7 +39,7 @@ bool check_collision_snake_food(SNAKE* snake, FOOD* food, SNAKE_SEG* ret_collide
 	return collided;
 }
 
-void refresh_food_position(SNAKE* snake, FOOD* food, int x_lim, int y_lim) {
+void refresh_food_position(FOOD* food, SNAKE* snake, int x_lim, int y_lim) {
 	SNAKE_SEG* ret_collided_seg = NULL;
 	change_food_position(food, x_lim, y_lim);
 	while (check_collision_snake_food(snake, food, ret_collided_seg)) {
--- GameSnake/Food.h
+++ GameSnake/Food.h
@@ -6,14 +6,14 @@
 //// STRUCT
 
 typedef struct {
-	int x;
-	int y;
+	int pos_x;
+	int pos_y;
 }FOOD;
 
 //// EXTERNAL FUNCTION DECLARATION
 
 bool check_collision_snake_food(SNAKE* snake, FOOD* food, SNAKE_SEG* ret_collided_seg);
 
-void refresh_food_position(SNAKE* snake, FOOD* food, int x_lim, int y_lim);
+void refresh_food_position(FOOD* food, SNAKE* snake, int x_lim, int y_lim);
 
 #endif
